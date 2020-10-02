e304520ef624725afa8def8368115f59ec6b7037
axldpir <danielpirs@googlemail.com>
2020-10-02 22:37:41 +0200

    Fetching data from firestore

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttershare/widgets/header.dart';
+
+final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -7,7 +11,35 @@ class Timeline extends StatefulWidget {
 
 class _TimelineState extends State<Timeline> {
   @override
+  void initState() {
+    //getUsers();
+    getUserByID();
+    super.initState();
+  }
+
+  // getUsers() {
+  //   usersRef.getDocuments().then((QuerySnapshot snapshot) {
+  //     snapshot.documents.forEach((DocumentSnapshot doc) {
+  //       print(doc.data);
+  //       print(doc.documentID);
+  //       print(doc.exists);
+  //     });
+  //   });
+  // }
+
+  getUserByID() async {
+    final String id = "Xc87a8HiEVkKAbSUm4H9";
+    final DocumentSnapshot doc = await usersRef.document(id).get();
+    print(doc.data);
+    print(doc.documentID);
+    print(doc.exists);
+  }
+
+  @override
   Widget build(context) {
-    return Text("Timeline");
+    return Scaffold(
+      appBar: header(context, isAppTitle: true),
+      body: LinearProgressIndicator(),
+    );
   }
 }
